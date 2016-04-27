374ce628d16ccece8b020f5f7012e95204912dc1
mohsen timar <mohsen.timar@gmail.com>
2016-04-27 10:27:30 +0430

    Add licence to unit

--- taskmanager.pas
+++ taskmanager.pas
@@ -1,3 +1,16 @@
+{
+  Copyright (C) 2012 Mohsen Timar mohsen.timar@gmail.com
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version with the following modification:
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+}
 unit TaskManager;
 
 {$mode objfpc}{$H+}
